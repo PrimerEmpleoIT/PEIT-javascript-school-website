49f970eabddc5c8acaf10533d515fb5d7f00739f
Conwa <conradodelcarlo97@gmail.com>
2022-11-06 15:44:28 -0300

    chequeo de selectores hero

--- src/mapeo.js
+++ src/mapeo.js
@@ -1,37 +1,34 @@
 const estadisticas = [
-    {
-        id:1,
-        name:"Years Experience",
-        numero:12,
-        clas:"fa-regular fa-calendar-days",
-    },
-    {
-        id:2,
-        name:"Total Achievment",
-        numero:4235,
-        clas:"fa-solid fa-medal"
-    },
-    {
-        id:3,
-        name:"Happy Student",
-        numero:3468,
-        clas:"fa-solid fa-child-dress",
-    },
-    {
-        id:4,
-        name:"Positive Review",
-        numero:"3k+",
-        clas:"fa-solid fa-comment-dots",
-    },
+  {
+    id: 1,
+    name: "Years Experience",
+    numero: 12,
+    clas: "fa-regular fa-calendar-days",
+  },
+  {
+    id: 2,
+    name: "Total Achievment",
+    numero: 4235,
+    clas: "fa-solid fa-medal",
+  },
+  {
+    id: 3,
+    name: "Happy Student",
+    numero: 3468,
+    clas: "fa-solid fa-child-dress",
+  },
+  {
+    id: 4,
+    name: "Positive Review",
+    numero: "3k+",
+    clas: "fa-solid fa-comment-dots",
+  },
 ];
 
+const createEstadisticas = (esta) => {
+  const { name, numero, clas } = esta;
 
-
-const createEstadisticas = (esta)=>{
-    const  {name ,numero,clas } = esta;
-    
-
-    return `
+  return `
     <div class="estadisticas">
     <i class="${clas}"></i>
         <div class="estadisticas-results">
@@ -41,14 +38,11 @@ const createEstadisticas = (esta)=>{
         </div>`;
 };
 
-
-const rendersEstadisticas = ()=>{
-    a.innerHTML = estadisticas.map(createEstadisticas).join("");
-
+const rendersEstadisticas = () => {
+  a.innerHTML = estadisticas.map(createEstadisticas).join("");
 };
-const init = () =>{
-
-rendersEstadisticas();
-}
+const init = () => {
+  rendersEstadisticas();
+};
 
 init();
