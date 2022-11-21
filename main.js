986e381ae0a1db844dc1d91d0a48b4cd7180db3f
Conwa <conradodelcarlo97@gmail.com>
2022-11-21 18:22:31 -0300

    hero arreglado para deploy final

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ import "./src/mapeo";
 import "./src/hamburguesa";
 import "./src/mapeo";
 
-import "./src/slider1"
-import "./src/slider2"
-import "./src/slider3"
-import "./src/slider4"
+// import "./src/slider1"
+// import "./src/slider2"
+// import "./src/slider3"
+// import "./src/slider4"
 
 
 console.log("main.js loaded");
